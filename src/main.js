0ad6ed1334b802fb02604180ce6654ef05223b54
ArsenZar <arsenzar@gmail.com>
2025-09-10 21:28:43 +0300

    create PWA script 0.3v (fix sw.js)

--- src/main.js
+++
@@ -1,5 +0,0 @@
-if ("serviceWorker" in navigator) {
-  navigator.serviceWorker.register("/sw.js")
-    .then(() => console.log("✅ Service Worker зареєстрований"))
-    .catch(err => console.error("❌ Помилка SW:", err));
-}
